f2fbfa19bc944df4e887126929f4cb72aaf7f3d4
Sara Hadi <sarahadialhadi@gmail.com>
2025-04-01 22:39:54 +0100

    Added private fields of the horse to the class

---
+++ part1/part1/Horse.java
@@ -0,0 +1,80 @@
+package part1;
+
+/**
+ * Write a description of class Horse here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Horse
+{
+    //Fields of class Horse
+    
+    
+      
+    //Constructor of class Horse
+    /**
+     * Constructor for objects of class Horse
+     */
+    public Horse(char horseSymbol, String horseName, double horseConfidence)
+    {
+       
+        public static void main(String[] args) {
+            System.out.println("Horse class is ready.");
+        }
+    }
+    
+    
+    
+    //Other methods of class Horse
+    public void fall()
+    {
+        
+    }
+    
+    public double getConfidence()
+    {
+        
+    }
+    
+    public int getDistanceTravelled()
+    {
+        
+    }
+    
+    public String getName()
+    {
+        
+    }
+    
+    public char getSymbol()
+    {
+        
+    }
+    
+    public void goBackToStart()
+    {
+        
+    }
+    
+    public boolean hasFallen()
+    {
+        
+    }
+
+    public void moveForward()
+    {
+        
+    }
+
+    public void setConfidence(double newConfidence)
+    {
+        
+    }
+    
+    public void setSymbol(char newSymbol)
+    {
+        
+    }
+    
+}
