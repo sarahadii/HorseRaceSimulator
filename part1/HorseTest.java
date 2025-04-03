8007e94492beb27e4c6c026e66af11f96eab8b2c
Sara Hadi <sarahadialhadi@gmail.com>
2025-04-03 01:59:53 +0100

    created 2 new files to test out both classes

---
+++ part1/HorseTest.java
@@ -0,0 +1,17 @@
+public class HorseTest {
+    public static void main(String[] args) {
+        Horse h = new Horse("Thunder", 'T', 0.75);
+        System.out.println("Horse Name: " + h.getName());
+        System.out.println("Horse Symbol: " + h.getSymbol());
+        System.out.println("Horse Confidence: " + h.getConfidence());
+        System.out.println("Distance: " + h.getDistanceTravelled());
+        System.out.println("Has Fallen: " + h.hasFallen());
+        h.moveForward();
+        System.out.println("Distance after moving forward: " + h.getDistanceTravelled());
+        h.fall();
+        System.out.println("Has Fallen: " + h.hasFallen());
+        h.goBackToStart();
+        System.out.println("Distance after going back to start: " + h.getDistanceTravelled());
+        System.out.println("Has Fallen: " + h.hasFallen());
+    }
+}
