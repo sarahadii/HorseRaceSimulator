cebe8c4647db801fa6666d6af524c64c3e936b17
Sara Hadi <sarahadialhadi@gmail.com>
2025-04-17 15:53:02 +0100

    Added a main method to test out

--- part2/MainHorsePage.java
+++ part2/MainHorsePage.java
@@ -9,4 +9,11 @@ public class MainHorsePage extends JFrame {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLayout(new BorderLayout());
     }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            MainHorsePage frame = new MainHorsePage();
+            frame.setVisible(true);
+        });
+    }
 }
