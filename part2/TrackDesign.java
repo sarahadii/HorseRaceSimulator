02a52dfc428d47e5433db8b958e9f012bcda98f9
Sara Hadi <sarahadialhadi@gmail.com>
2025-04-17 20:45:44 +0100

    Added a frame and a title

--- part2/TrackDesign.java
+++ part2/TrackDesign.java
@@ -1,13 +1,32 @@
 package part2;
 
+import java.awt.BorderLayout;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JSlider;
 import javax.swing.JTextArea;
+import javax.swing.SwingUtilities;
 
 public class TrackDesign extends JFrame{
     JSlider lengthSlider;
     JSlider laneSlider;
     JComboBox<String> shapeBox;
     JTextArea preview; 
+    
+    public TrackDesign(){
+        setTitle("Track Design");
+        setSize(600,400);
+        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        setLocationRelativeTo(null);
+        setLayout(new BorderLayout());
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            TrackDesign frame = new TrackDesign();
+            frame.setVisible(true);
+        });
+    }
+
 }
