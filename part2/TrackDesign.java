df9a25912220ab966819179c4bd3361e96772542
Sara Hadi <sarahadialhadi@gmail.com>
2025-04-17 20:40:27 +0100

    Created new variables for the designs

--- part2/TrackDesign.java
+++ part2/TrackDesign.java
@@ -1,5 +1,13 @@
 package part2;
 
-public class TrackDesign {
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JSlider;
+import javax.swing.JTextArea;
 
+public class TrackDesign extends JFrame{
+    JSlider lengthSlider;
+    JSlider laneSlider;
+    JComboBox<String> shapeBox;
+    JTextArea preview; 
 }
