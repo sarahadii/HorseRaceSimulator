411447394762933557cfb83b22b199aa7513215e
Sara Hadi <sarahadialhadi@gmail.com>
2025-04-20 01:45:52 +0300

    Added text and buttons on the top of the screen

--- part2/horseCustbutton.java
+++ part2/horseCustbutton.java
@@ -1,5 +1,18 @@
 package part2;
 
-public class horseCustbutton {
+import java.awt.Button;
+
+import javax.swing.JFrame;
+
+public class horseCustbutton extends JFrame {
+    Button breedButton;
+    // combo box color,symbol text ,combo box accessories, attributes
+    public horseCustbutton() {
+        setTitle("Horse Customization");
+        setSize(400, 300);
+        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        setLocationRelativeTo(null);
+        setLayout(null);
+    }
 
 }
