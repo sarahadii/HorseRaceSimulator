d07e7e797bccf89d10f3987ed6ef6c52953be1c0
Sara Hadi <sarahadialhadi@gmail.com>
2025-04-20 01:51:14 +0300

    Created a new set of buttons to select the breed

--- part2/horseCustbutton.java
+++ part2/horseCustbutton.java
@@ -1,8 +1,14 @@
 package part2;
 
+import java.awt.BorderLayout;
 import java.awt.Button;
+import java.awt.GridLayout;
 
+import javax.swing.BorderFactory;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JRadioButton;
 
 public class horseCustbutton extends JFrame {
     Button breedButton;
@@ -13,6 +19,29 @@ public class horseCustbutton extends JFrame {
         setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         setLocationRelativeTo(null);
         setLayout(null);
+
+        JPanel centerPanel = new JPanel();
+        centerPanel.setLayout(new GridLayout(5, 1, 10, 10));
+        centerPanel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+
+        JPanel breedPanel = new JPanel();
+        JLabel breedLabel = new JLabel("Horse breed:");
+        breedPanel.add(breedLabel,BorderLayout.WEST);
+        breedButton = new Button();
+        JRadioButton arabian  = new JRadioButton("Arabian");
+        JRadioButton thoroughbred = new JRadioButton("Thoroughbred");
+        JRadioButton quarterHorse = new JRadioButton("Quarter Horse");
+        JRadioButton appaloosa = new JRadioButton("Appaloosa");
+
+        breedPanel.add(arabian);
+        breedPanel.add(thoroughbred);
+        breedPanel.add(quarterHorse);
+        breedPanel.add(appaloosa);
+        centerPanel.add(breedPanel, BorderLayout.NORTH);
+        add(centerPanel, BorderLayout.CENTER);
+
+        
+
     }
 
 }
