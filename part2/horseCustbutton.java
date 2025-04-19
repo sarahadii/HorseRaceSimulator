f154ad82370947492c95f54c18f9fbd8a7ac8bbb
Sara Hadi <sarahadialhadi@gmail.com>
2025-04-20 02:01:54 +0300

    Fixed an issue with the breedOanel since it wasnt showing up

--- part2/horseCustbutton.java
+++ part2/horseCustbutton.java
@@ -5,20 +5,22 @@ import java.awt.Button;
 import java.awt.GridLayout;
 
 import javax.swing.BorderFactory;
+import javax.swing.ButtonGroup;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JRadioButton;
+import javax.swing.SwingUtilities;
 
 public class horseCustbutton extends JFrame {
     Button breedButton;
     // combo box color,symbol text ,combo box accessories, attributes
     public horseCustbutton() {
         setTitle("Horse Customization");
-        setSize(400, 300);
+        setSize(700, 600);
         setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         setLocationRelativeTo(null);
-        setLayout(null);
+        setLayout(new BorderLayout()); 
 
         JPanel centerPanel = new JPanel();
         centerPanel.setLayout(new GridLayout(5, 1, 10, 10));
@@ -27,21 +29,31 @@ public class horseCustbutton extends JFrame {
         JPanel breedPanel = new JPanel();
         JLabel breedLabel = new JLabel("Horse breed:");
         breedPanel.add(breedLabel,BorderLayout.WEST);
-        breedButton = new Button();
         JRadioButton arabian  = new JRadioButton("Arabian");
         JRadioButton thoroughbred = new JRadioButton("Thoroughbred");
         JRadioButton quarterHorse = new JRadioButton("Quarter Horse");
         JRadioButton appaloosa = new JRadioButton("Appaloosa");
 
+        ButtonGroup breedGroup = new ButtonGroup();
+        breedGroup.add(arabian);
+        breedGroup.add(thoroughbred);
+        breedGroup.add(quarterHorse);
+        breedGroup.add(appaloosa);
+        
         breedPanel.add(arabian);
         breedPanel.add(thoroughbred);
         breedPanel.add(quarterHorse);
         breedPanel.add(appaloosa);
-        centerPanel.add(breedPanel, BorderLayout.NORTH);
+        centerPanel.add(breedPanel, BorderLayout.SOUTH);
         add(centerPanel, BorderLayout.CENTER);
 
-        
-
     }
-
+        
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            horseCustbutton frame = new horseCustbutton();
+            frame.setVisible(true);
+        });
+    }
 }
+
